b2ff982ee4b9c4b8f6b4493697746798749ca3e1
Nelson Gacheru <nelsonzachariah@gmail.com>
2019-02-25 11:29:50 +0300

    include toggle && edit button,

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Business logic
 $(document).ready(function(){
-  $("input[type='button']").click (function (event) {
+  $("button#answer").click (function (event) {
 
     var question1 = parseInt($("input[name='abb']:checked").val());
     var question2 = parseInt($("input[name='ques2']:checked").val());
@@ -12,12 +12,26 @@ $(document).ready(function(){
     var question8 = parseInt($("input[name='ques8']:checked").val());
     var question9 = parseInt($("input[name='ques9']:checked").val());
     var question10 = parseInt($("input[name='ques10']:checked").val());
-
-    //user interface logic
-
       var total = question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10
 
       document.getElementById("output").innerHTML = "Hurrrayyyyy you have scored " + total+ "%";
+      document.getElementByClass("progress").innerHTML = total;
+alert(total)
+
 
   });
+});
+
+
+
+///User interface ;
+$(document).ready(function(){
+  $("button#start").click(function(){
+    $("form.details").slideUp();
+    $("div.container").toggle(3000);
+  });
+//  $("button#answer").click(function(){
+  //  var current = $("#prog").val();
+    //var new = Number(current) + total;
+    //$("#prog").val(new);
 });
